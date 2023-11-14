27430ce0d5a8db5a9335639fc2a8c05f8f33ce48
parthkal <97428134+parthkal@users.noreply.github.com>
2023-11-14 14:00:17 -0700

    Added new files

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Parth K.");
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});

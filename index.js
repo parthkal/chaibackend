725787c362019b33931f41db71e9dde1dcc15151
parthkal <97428134+parthkal@users.noreply.github.com>
2023-11-14 14:25:22 -0700

    Added /parth

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/parth", (req, res) => {
+  res.json({
+    firstName: "Parth",
+    lastName: "Kalra",
+    email: "parth.kalra1712@gmail.com",
+  });
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
